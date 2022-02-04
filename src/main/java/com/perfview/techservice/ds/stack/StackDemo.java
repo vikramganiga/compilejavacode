c7a888038538cfaeb332bf09555a47c5881890d8
vikramganiga <vikramganiga@gmail.com>
2022-02-04 08:16:17 +0530

    stack

---
+++ src/main/java/com/perfview/techservice/ds/stack/StackDemo.java
@@ -0,0 +1,25 @@
+package com.perfview.techservice.ds.stack;
+
+public class StackDemo {
+
+	public static void main(String args[]) {
+		Stack stack = new Stack();
+		
+		try {
+			stack.push(10);
+			stack.push(20);
+		} catch (StackException e) {
+			System.out.println(e.getError());
+			e.printStackTrace();
+		}
+		try {
+			System.out.println(stack.pop());
+			System.out.println(stack.pop());
+			System.out.println(stack.pop());
+		} catch (StackException e) {
+			System.out.println(e.getError());
+			e.printStackTrace();
+		}
+		
+	}
+}
---
+++ src/main/java/com/perfview/techservice/ds/stack/StackException.java
@@ -0,0 +1,17 @@
+package com.perfview.techservice.ds.stack;
+
+public class StackException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+	private String error;
+		
+	public StackException(String error) {
+		super();
+		this.error = error;
+	}
+	
+	public String getError() {
+		return error;
+	}
+
+}
