95f2763fc092673088e24373e69cb83a0c37ee61
vikramganiga <vikramganiga@gmail.com>
2022-09-24 17:03:34 +0530

    observer pattern - comment updated

--- src/test/java/com/ds/linkedList/TestCompareLinkedList.java
+++ src/test/java/com/ds/linkedList/TestCompareLinkedList.java
@@ -6,11 +6,7 @@ import com.core.linkedlist.ProcessLinkedList;
 import com.core.linkedlist.SampleLinkedList;
 
 /**
- * This is an to practice traversing a linked list.
- * Given a pointer to the head node of a linked list,
- * print each node's element, one per line.
- * If the head pointer is null (indicating the list is empty)
- * else , there is nothing to print.
+ * Compare two linkedList
  */
 
 class TestCompareLinkedList {
